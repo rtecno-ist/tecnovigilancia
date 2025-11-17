e4742f0b26089da9350bbda2f8618527f11279e4
Aldo Carquín <aldo.gonzalez.carquin@gmail.com>
2025-11-17 05:19:48 -0300

    Fix responsive template

--- formulario/urls.py
+++ formulario/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("", views.registrar_incidente, name="form_incidente"),
     path("success/<int:pk>/", views.success, name="success"),
     path('incidentes/', views.lista_incidentes, name='lista_incidentes'),
+
 ]
